ef927bea2220fcdbed8b65826ac536e487736c8d
userManny <maneeshkumarbhu26@gmail.com>
2026-05-06 20:56:11 +0530

    Stylized UserCards

--- src/Components/UserCard/UserCard.jsx
+++ src/Components/UserCard/UserCard.jsx
@@ -7,18 +7,42 @@ function UserCard({name,email,phone,plan,paid,onMarkPaid,onDelete}){
 
     return(
         <>
-        <div className="card">
-        <h3>{name}</h3>
-        <p>{email}</p>
-        <p>{`${plan} + (${selectedPlan.speed})`}</p>
-        <p>₹{selectedPlan.bill}</p>
-        <p>{paid? "Paid":"Due"}</p>
-        <button onClick={onMarkPaid}
-        className={paid? "paid-btn":"unpaid-btn"}
-        >
-        {paid?"Payment Received":"Mark as Paid"}
+        <div className="user-card">
+            <div className="card-header">
+                 <h3>{name}</h3>
+                <p>{email}</p>
+            </div>
+            <div className="user-details">
+                <div className="detail-row">
+                    <span>Plan</span>
+                    <span>{plan}</span>
+                </div>
+
+                <div className="detail-row">
+                    <span>Speed</span>
+                    <span>{selectedPlan.speed}</span>
+                </div>
+
+                <div className="detail-row">
+                    <span>Monthly</span>
+                    <span>₹{selectedPlan.bill}</span>
+                </div>
+            </div>
+            <div className="status">
+                <span>Status</span>
+                <span className={paid?"paid-status":"due-status"}>{paid? "Paid":"Due"}</span>
+            </div>
+            <div className="btn-group">
+                       <button onClick={onMarkPaid}
+                        className={paid? "paid-btn":"unpaid-btn"}
+                        >
+                  {paid?"Payment Received":"Mark as Paid"}
              </button>
-          <button className="delete-user-btn" onClick={onDelete}>Delete User</button>
+            <button className="delete-user-btn" onClick={onDelete}>Delete User</button>
+            </div>
+       
+        
+  
         </div>
        
         </>
